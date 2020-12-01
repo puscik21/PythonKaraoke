1bc3aeb139855ad466d64046de19d1a9c4f8a334
puscik21 <grzesiekpuscizna@gmail.com>
2020-12-01 15:59:33 +0100

    new Procfile

--- music_karaoke/music_karaoke/asgi.py
+++ music_karaoke/music_karaoke/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_karaoke.music_karaoke.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_karaoke.settings')
 
 application = get_asgi_application()
