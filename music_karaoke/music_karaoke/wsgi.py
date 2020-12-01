51ef7404fcf6ef121ac918286fa01011a1596cd7
puscik21 <grzesiekpuscizna@gmail.com>
2020-12-01 15:52:07 +0100

    new Procfile

--- music_karaoke/music_karaoke/wsgi.py
+++ music_karaoke/music_karaoke/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_karaoke.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_karaoke.music_karaoke.settings')
 
 application = get_wsgi_application()
